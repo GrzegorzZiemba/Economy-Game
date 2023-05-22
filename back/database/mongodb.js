a37dfc7df3cf503a0ecad352102492013617a02e
Grzesiek <gzgrzegorzziemba@gmail.com>
2023-05-22 14:48:59 +0200

    Commit with ignored files properly ignored.

--- back/database/mongodb.js
+++ back/database/mongodb.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose')
-const config = require('config')
+const mongoose = require("mongoose");
+const config = require("config");
+require("dotenv").config();
 
-module.exports = function createMongoDb(){
-    mongoose.connect(config.get('mongoUrl')).then(result=>{console.log("Connected")}).catch(err=> console.log(err))
-}
+module.exports = function createMongoDb() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((result) => {
+      console.log("Connected");
+    })
+    .catch((err) => console.log(err));
+};
--- back/routes/accountRoutes.js
+++ back/routes/accountRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createAccountController } = require("../controllers/accountController");
 const Account = require("../dbSchemas/account");
 const Resources = require("../dbSchemas/resources");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
@@ -23,6 +24,19 @@ router.post("/login", async (req, res) => {
     res.status(200).send({ mail, token, id });
   } catch (e) {
     res.status(400).send({ error: "Cannot Login" });
+  }
+});
+
+router.post("/logout", async (req, res) => {
+  const userId = JSON.parse(req.body.id);
+  const id = mongoose.Types.ObjectId(userId);
+  try {
+    const user = await Account.findById({ _id: id });
+    user.tokens = "";
+    user.save();
+    res.status(200).send({ text: "Logged Out!" });
+  } catch (error) {
+    res.status(400).send({ error: "Cannot Logout" });
   }
 });
 
